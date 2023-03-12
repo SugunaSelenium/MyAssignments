51f00704e4b6171a602aab34778c82eabb36c40f
SugunaSelenium <127003584+SugunaSelenium@users.noreply.github.com>
2023-03-12 11:32:42 +0800

    Add files via upload
    Week2-Day1 assignments

---
+++ day1/BasicLocators.java
@@ -0,0 +1,25 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicLocators {
+
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("DemoCSR");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.partialLinkText("CRM")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Suguna");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Senthil");
+		driver.findElement(By.name("submitButton")).click();
+
+	}
+
+}
