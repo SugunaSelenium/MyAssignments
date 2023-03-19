30758f9150bc7ca8dbf4e51752851d4d41e4cbfe
SugunaSelenium <127003584+SugunaSelenium@users.noreply.github.com>
2023-03-19 13:31:29 +0800

    Add files via upload
    Week 3 day 1

---
+++ week3/day1/MyPhone.java
@@ -0,0 +1,14 @@
+package week3.day1;
+
+public class MyPhone {
+
+	public static void main(String[] args) {
+		iPhone app=new iPhone();
+		app.startApp();
+		app.increaseVolume();
+		app.shutdown();
+		app.makeCall();
+		app.sendSMS();
+			}
+
+}
